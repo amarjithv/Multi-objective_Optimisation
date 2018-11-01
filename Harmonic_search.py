bf1ad5f52f92f74a6972de86bd004d8d26f3f36e
amarjithv <amarjith@iitg.ernet.in>
2018-11-01 20:05:53 +0530

    Beta version completed

--- Harmonic_search.py
+++ Harmonic_search.py
@@ -2,81 +2,85 @@
 import numpy as np
 import scipy as scp
 import enum
-
+from pareto import non_dominated_sorting
 from problem import obj_function
-def find_worst(funct, n):
+from pareto import find_rank
+def find_worst(ranks):
     worst = 0
-    for i in range(funct.shape[0]):
-        if n==1:
-            if funct[i] < funct[worst]:
-                worst = i
-        else:
-            if funct[i] > funct[worst]:
-                worst = i
+    for i in range(ranks.shape[0]):
+        if ranks[i] > ranks[worst]:
+            worst = i
     return worst
 
-
+    
 #Ennumerate
 class Objectives(enum.Enum):
     maximize = 1
     minimize = 0
 
 #problem_statement
-upper_bound = 10
-lower_bound = -10
+upper_bound = 10^3
+lower_bound = -10^3
+upper_bound1 = 10^3
+lower_bound1 = -10^3
 question_no = 1 
 
 #Parameter configuration
 hmcr = 0.8
-hms = 10000
-mi = 10000
+hms = 250
+mi = 200
 par = 0.3
 bw = 2
-no_of_var = 3
+no_of_var = 1
 objective = 0
+M = 2
 
 #Memory Initialization
-hm = np.random.rand(hms, no_of_var)*(upper_bound-lower_bound)+lower_bound
+hm = np.random.rand(hms, no_of_var)*(upper_bound1-lower_bound1)+lower_bound1
+#hm[5]=[11, 11]
 new_soln = np.zeros(no_of_var)
-function_value = np.zeros(hms)
+function_value = np.zeros((hms, M))
 
 #hm[5] = [0, 0]
 
 for i in range(hms):
-    function_value[i] = obj_function(hm[i], question_no)#define the function
+    function_value[i] = obj_function(hm[i], question_no).flatten()#define the function
 
+rank = non_dominated_sorting(function_value)
+print(rank)
+import sys
+#sys.exit()
 
 while(mi>0):
+    rank = non_dominated_sorting(function_value)
     for i in range(no_of_var):
 
         uniform_distribution = np.random.random_sample()
-        print(uniform_distribution)
         if uniform_distribution < hmcr:
             j = int(uniform_distribution*hms)+1
-            print(j)
             new_soln[i] = hm[j, i]
             if uniform_distribution < par:
                 delta = (np.random.random_sample()*2 - 1) * bw
                 new_soln[i] = new_soln[i] + delta
         else:
-            new_soln[i] = lower_bound + uniform_distribution*(upper_bound-lower_bound)#edit
+            new_soln[i] = lower_bound + np.random.random_sample()*(upper_bound-lower_bound)#edit
 
-    worst_soln_index = find_worst(function_value, 1)
-    if objective == 1:
-        if obj_function(new_soln, question_no) > function_value[worst_soln_index]:
-            function_value[worst_soln_index] = obj_function(new_soln, question_no)
-            hm[i] = new_soln
-    else:
-        if obj_function(new_soln, question_no) < function_value[worst_soln_index]:
-            function_value[worst_soln_index] = obj_function(new_soln, question_no)
-            hm[i] = new_soln
-    
+    worst_soln_index = find_worst(rank)
+    new_sol_fval = obj_function(new_soln, question_no).flatten()
+    new_sol_rank = find_rank(function_value, new_sol_fval)
+    if new_sol_rank < rank[worst_soln_index]:
+        hm[worst_soln_index] = new_soln
+        function_value[worst_soln_index] = new_sol_fval
     mi = mi - 1
 
-if objective == 1:
-    soln_index = np.argmax(function_value, axis=0)
-else:
-    soln_index = np.argmin(function_value, axis=0)
+print(rank)
+import matplotlib.pyplot as plt
+plt.plot(hm[:,0], hm[:, 1],'ro')
+plt.show()
 
-solution = hm[soln_index]
-print(solution)
+a=[]
+for i in range(hms):
+    if(rank[i]==1):
+        a.append(hm[i])
+plt.plot(a,'ro')
+plt.show()
--- problem.py
+++ problem.py
@@ -1,15 +1,38 @@
 #!/usr/bin/env python3
 import numpy as np 
+import math
 
-def obj_function(x, qno):
-    fx=0
-    if qno == 1:
-        for i in range(x.shape[0]):
-            fx = fx + (x[i]-1)**2
-        for i in range(x.shape[0]-1):
-            fx = fx - x[i]*x[i+1]
-    elif qno == 2:
-        for i in range(x.shape[0]):
-            fx = fx + (i+1)*(x[i]**2)
+def modulus( y ):
+    m=0
+    for yi in y:
+        m = m+yi^2
+    return math.sqrt(m)
+
+def obj_function1(x, qno):
+    f1x = 0.5
+    f2x=0.5
+    gx=0
+    for xi in x[:2]:
+        gx = gx+((xi-0.5)*(xi-0.5)-math.cos(20*math.pi*(xi-0.5)))
+    for i in range(1):
+        f1x = f1x * x[i]
+        f2x = f2x * (1-x[i])
+    gx = modulus(x[2:])+gx
+    gx = gx*100
+    f1x = f1x*(1+gx)
+    f2x = f2x*(1+gx)
+    fx = []
+    fx.append(f1x)
+    fx.append(f2x)
+    fx = np.array(fx)
     return fx
-
+    
+def obj_function(x, qno):
+    f1x=x*x
+    f2x=(x-2)*(x-2)
+    fx = []
+    fx.append(f1x)
+    fx.append(f2x)
+    fx = np.array(fx)
+    print(fx.shape)
+    return fx 
